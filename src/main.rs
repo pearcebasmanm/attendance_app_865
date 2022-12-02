4f07ec110345fd4337dc1584668c0b60cc91ad75
Max <pearcebasmanm@gmail.com>
2022-12-01 20:35:18 -0500

    Move to main

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use leptos::*;
+
 fn main() {
-    leptos::mount_to_body(attendance_app_865::attendance_app)
+    leptos::mount_to_body(attendance_app)
+}
+
+
+pub fn attendance_app(cx: Scope) -> Element {
+    let (counter, set_counter) = create_signal(cx, 0);
+    view! {
+        cx,
+        <div>
+        <h1>"Attendance App"</h1>
+        <h2>"Counter is at: " {counter}</h2>
+        <button on:click=move |_| set_counter.update(|val| *val -= 1)>"-1"</button>
+        <button on:click=move |_| set_counter.update(|val| *val += 1)>"+1"</button>
+        </div>
+    }
 }
