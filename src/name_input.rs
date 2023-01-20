c32f10efa3c85a34bea7d41bce672a7bea8d7577
Max Pearce Basman <pearcebasmanm@gmail.com>
2023-01-20 15:53:16 -0500

    update

--- src/name_input.rs
+++ src/name_input.rs
@@ -1,38 +1,48 @@
-use leptos::*;
-use crate::gsheets;
+use crate::gsheets::get_names;
+use leptos::{web_sys::KeyboardEvent, *};
 
 const TAB: u32 = 9;
+const ENTER: u32 = 13;
 
 #[component]
 pub fn NameInput(cx: Scope) -> Element {
     let (name, set_name) = create_signal(cx, String::new());
-    let (suggestion, set_suggestion) = create_signal(cx, String::new());
+    let (hint, set_hint) = create_signal(cx, String::new());
 
-    let names = gsheets::get_names();
+    let names = get_names();
 
-    view!{
+    let on_keydown = move |ev: KeyboardEvent| {
+        let input = event_target_value(&ev);
+        let hint = if input.trim().is_empty() {
+            String::new()
+        } else {
+            match names.iter().find(|name| name.contains(&input)) {
+                Some(val) => val.clone(),
+                None => String::new(),
+            }
+        };
+
+        match ev.key_code() {
+            TAB => {
+                set_name(hint);
+                set_hint("Accepted Suggestion".to_string());
+            }
+            ENTER => {
+                set_name(input);
+                set_hint("Accepted Input".to_string())
+            }
+            _ => set_hint(hint),
+        }
+    };
+
+    view! {
         cx,
         <div>
         <p>{name}</p>
         <input
             placeholder="Input Name Here"
-            on:keydown=move |ev| {
-                let written = event_target_value(&ev);
-                let suggested = names
-                    .iter()
-                    .find(|name| name.contains(&written));
-                
-                if let Some(suggested) = suggested {     
-                    if ev.key_code() == TAB {
-                        set_name(suggested.to_string());
-                        set_suggestion("Accepted Suggestion".to_string());
-                    } else {
-                        set_suggestion(suggested.to_string());
-                    }
-                }
-            }
-        />
-        <p>{suggestion}</p>
+            on:keydown=on_keydown/>
+        <p>{hint}</p>
         </div>
     }
 }
